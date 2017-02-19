cea008488ba1b206a7feb3d2be18b7e8e84b03b1
nahin00 <contact.nahin@gmail.com>
2017-02-19 20:45:54 +0600

    Added some extra features

--- JSONSerialization/ViewController.swift
+++ JSONSerialization/ViewController.swift
@@ -12,7 +12,50 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "https://api.adorable.io/avatars/list")
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            if error != nil {
+                print("Error")
+            } else {
+                
+                if let allData = data{
+                    do{
+                        let allJSONData = try JSONSerialization.jsonObject(with: allData, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        
+                        // Prints all JSON data
+                        
+                        print(allJSONData)
+                        
+                        if let face = allJSONData["face"] as AnyObject? {
+                            
+                            // Prints face
+                            
+                            print(face)
+                            
+                            if let eyes = face["eyes"] as! NSArray? {
+                                
+                                // Prints eyes
+                                
+                                print(eyes)
+                                
+                                for i in 0..<3 {
+                                    
+                                    // Prints first 3 eyes
+                        
+                                    print(eyes[i])
+                                }
+                            }
+                        }
+                    } catch {
+                        
+                    }
+                }
+            }
+        }
+        
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
